a47b6b5a97c5c10b92b7e054ea6182fac17cbed2
vramirez4 <49317034+vramirez4@users.noreply.github.com>
2019-04-05 09:15:28 -0700

    Delete CleanTemporaryFiles.R

--- Inia_Genomic_Toolkit/CleanTemporaryFiles.R
+++
@@ -1,7 +0,0 @@
-#REMOVE TEMPORARY FILES CREATED BY VCFTOOLS
-CleanFiles=function(csv) {
-  for (i in seq(1, dim(csv)[1])) {
-    cleaner = sprintf("rm -r tmp_%s*", csv[i, 1])
-    system(cleaner, ignore.stdout = TRUE)
-  }
-}
